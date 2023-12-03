a30738f41229ec07950154fc3907bbcea85ab55c
udongmandu <qjawns10000@naver.com>
2023-12-03 11:45:45 +0900

    관리자 페이지 업데이트

--- node/sources/src/pages/login/signIn.jsx
+++ node/sources/src/pages/login/signIn.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import Button from "../../Components/ButtonTop";
 
 export default function LoginPage() {
+  function loging(e) {
+    //로그인 체크 기능 없음
+    e.preventDefault();
+    const inputID = e.target.elements.ID.value;
+    // const inputPW = e.target.elements.PASSWORD.value;
+
+    //로컬 스토리지에 정보 로그인 정보 저장
+    const userData = { name: inputID, author: "admin" };
+    localStorage.setItem("user", JSON.stringify(userData));
+
+    window.location.href = "/student";
+  }
+
   return (
     <>
       <div className="absolute w-[100vw] h-[100vh] top-0 left-0 bg-black opacity-50" />
@@ -12,11 +25,11 @@ export default function LoginPage() {
             <Button
               label={"X"}
               width={50}
-              URL={"/"}
+              URL={"/student"}
               bgColor={"5272F2"}
             ></Button>
           </div>
-          <form action="" className="flex justify-center">
+          <form onSubmit={(e) => loging(e)} className="flex justify-center">
             <div className="flex flex-col">
               <input
                 className="px-2 w-80 h-11 rounded-md border border-[#000000] my-7"
@@ -29,14 +42,14 @@ export default function LoginPage() {
                 placeholder={`비밀번호`}
               />
               <div className="flex justify-evenly pt-6">
+                <button className="text-xs w-20 h-10 px-2 rounded-md border bg-[#5272F2] text-white">
+                  로그인
+                </button>
                 <Button
                   label={"신규등록"}
                   width={80}
                   URL={"/register-page"}
                 ></Button>
-                <button className="text-xs w-20 h-10 px-2 rounded-md border bg-[#5272F2] text-white">
-                  로그인
-                </button>
               </div>
               <span className="text-xs font-extrabold pt-3 text-center">
                 신규등록을 통해 새로운 교사의 계정 추가 가능
