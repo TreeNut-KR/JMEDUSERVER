4859accd8ff4a50ac08fb67390e60ebd07e09872
udongmandu <qjawns10000@naver.com>
2023-12-03 11:45:45 +0900

    관리자 페이지 업데이트

--- node/sources/src/Components/dataTableV1/DataTableV1.jsx
+++ node/sources/src/Components/dataTableV1/DataTableV1.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
+import moreIcon from "../../img/pending-icon.png";
 
 export default function DataTableV1(props) {
-  const { styleClass, datas, columns, title } = props;
+  const { styleClass, datas, columns, title, type } = props;
   const [currentPage, setCurrentPage] = React.useState(1);
   const totalNumber = datas.length;
+
+  //자세히 보기 기능
+  const [expandedRowIndex, setExpandedRowIndex] = useState(-1);
+
   //페이지당 보일 데이터 갯수 나중에 고를 수 있도록 수정할지도
   const itemsPerPage = 10;
 
@@ -14,8 +19,42 @@ export default function DataTableV1(props) {
   const endIndex = startIndex + itemsPerPage;
   const currentPageData = datas.slice(startIndex, endIndex);
 
+  //테이블 내의 '자세히 보기' 버튼 뜨도록 하는 기능 -------
+  const toggleRowExpansion = (index) => {
+    if (expandedRowIndex === index) {
+      setExpandedRowIndex(-1);
+    } else {
+      setExpandedRowIndex(index);
+    }
+  };
+
+  const closeExpandedRow = () => {
+    setExpandedRowIndex(-1);
+  };
+
+  const tableRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (tableRef.current && !tableRef.current.contains(event.target)) {
+        closeExpandedRow();
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  function buttonEffect(ID) {
+    const editUrl = `/${type}-edit/${ID}`;
+    window.location.href = editUrl;
+  }
+  // -------------------------------------------------------------------------
   return (
-    <div className="w-full p-10 pt-0">
+    <div ref={tableRef} className="w-full p-10 pt-0">
       <div className="border border-[#B3A492] rounded-md">
         <table
           className={`${styleClass} border-collapse rounded-md text-sm shadow-md w-full fontA `}
@@ -35,23 +74,44 @@ export default function DataTableV1(props) {
           {totalNumber > 0 ? (
             <tbody className="px-3">
               {currentPageData.map((item, index) => (
-                <tr key={startIndex + index}>
-                  {columns.map((column, columnIndex) => {
-                    if (column.data === "no") {
-                      return (
-                        <td className="py-2 px-2" key={columnIndex}>
-                          {startIndex + index + 1}
-                        </td>
-                      );
-                    } else {
-                      return (
-                        <td className="py-2 px-2" key={columnIndex}>
-                          {item[column.data]}
-                        </td>
-                      );
-                    }
-                  })}
-                </tr>
+                <React.Fragment key={startIndex + index}>
+                  <tr className="relative">
+                    {columns.map((column, columnIndex) => {
+                      if (column.data === "no") {
+                        return (
+                          <td className="py-2 px-2 border-b" key={columnIndex}>
+                            {startIndex + index + 1}
+                          </td>
+                        );
+                      } else {
+                        return (
+                          <td className="py-2 px-2 border-b" key={columnIndex}>
+                            {item[column.data]}
+                          </td>
+                        );
+                      }
+                    })}
+                    <td className="absolute right-1 top-[6px]">
+                      <button
+                        className="relative"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          toggleRowExpansion(index);
+                        }}
+                      >
+                        <img src={moreIcon} alt="" />
+                        {expandedRowIndex === index && (
+                          <span
+                            className="absolute top-[-4px] left-[7rem] transform translate-x-[-50%] bg-[#5272F2] px-3 py-1 border rounded w-[10rem] text-white"
+                            onClick={() => buttonEffect(index + 1)}
+                          >
+                            자세히 보기 / 수정
+                          </span>
+                        )}
+                      </button>
+                    </td>
+                  </tr>
+                </React.Fragment>
               ))}
             </tbody>
           ) : (
@@ -59,54 +119,56 @@ export default function DataTableV1(props) {
           )}
         </table>
       </div>
-      <div className="flex justify-end mt-2">
+      <div className="flex justify-end mt-2 relative items-center">
         {currentPage > 1 && (
           <>
             <button
               onClick={() => setCurrentPage(1)}
-              className={`px-2 rounded-lg border bg-white`}
+              className={`px-2 rounded-lg border bg-white absolute right-56 select-none`}
             >
               {"<<"}
             </button>
             <button
               onClick={() => setCurrentPage(currentPage - 1)}
-              className={`px-2 rounded-lg border bg-white`}
+              className={`px-2 rounded-lg border bg-white absolute right-48 select-none`}
             >
               {"<"}
             </button>
           </>
         )}
-        {Array.from({ length: totalPages }, (_, index) => {
-          const pageNumber = index + 1;
-          if (Math.abs(currentPage - pageNumber) <= 2) {
-            return (
-              <button
-                key={index}
-                onClick={() => setCurrentPage(pageNumber)}
-                className={`px-2 mx-1 rounded-lg border text-xs ${
-                  currentPage === pageNumber
-                    ? "bg-[#5272F2] text-white"
-                    : "bg-white"
-                }`}
-              >
-                {pageNumber}
-              </button>
-            );
-          } else {
-            return null;
-          }
-        })}
+        <div className="pr-[80px]">
+          {Array.from({ length: totalPages }, (_, index) => {
+            const pageNumber = index + 1;
+            if (Math.abs(currentPage - pageNumber) <= 2) {
+              return (
+                <button
+                  key={index}
+                  onClick={() => setCurrentPage(pageNumber)}
+                  className={`px-2 mx-1 rounded-lg border text-xs h-7 w-7 select-none ${
+                    currentPage === pageNumber
+                      ? "bg-[#5272F2] text-white"
+                      : "bg-white"
+                  }`}
+                >
+                  {pageNumber}
+                </button>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
         {currentPage < totalPages && (
           <>
             <button
               onClick={() => setCurrentPage(currentPage + 1)}
-              className={`px-2 rounded-lg border bg-white`}
+              className={`px-2 rounded-lg border bg-white absolute right-11 select-none`}
             >
               {">"}
             </button>
             <button
               onClick={() => setCurrentPage(totalPages)}
-              className={`px-2 rounded-lg border bg-white`}
+              className={`px-2 rounded-lg border bg-white absolute right-0 select-none`}
             >
               {">>"}
             </button>
@@ -122,4 +184,5 @@ DataTableV1.propTypes = {
   datas: PropTypes.array,
   styleClass: PropTypes.string,
   columns: PropTypes.array.isRequired,
+  type: PropTypes.string.isRequired,
 };
