c94b56c2797f157af365591ffdea48d5676106ad
udongmandu <qjawns10000@naver.com>
2023-12-23 11:35:51 +0900

    로고 이미지 수정

--- node/sources/src/template/Topbar.jsx
+++ node/sources/src/template/Topbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../style/index.css";
 import Button from "../Components/ButtonTop";
-import LOGO from "../img/logoEX.png";
+import LOGO from "../img/logo_final_2.png";
 
 export default function Topbar() {
   const [user, setUser] = useState(null);
@@ -20,20 +20,27 @@ export default function Topbar() {
 
   return (
     <div className="w-full h-24 flex items-center justify-between px-16 bg-[#F3F4F6] border-b-2 text-slate-500">
-      <img
-        className="w-52 cursor-pointer"
-        src={LOGO}
-        alt=""
-        onClick={() => {
-          window.location.href = "/student";
-        }}
-      />
-      {!user ? (
-        <Button
-          URL="/sign-in"
-          label="로그인"
-          styleClass={"w-28 h-11 border-[3px] rounded-lg fontA text-ms"}
+      <div className="py-4">
+        <img
+          className="w-52 cursor-pointer"
+          src={LOGO}
+          alt=""
+          onClick={() => {
+            window.location.href = "/student";
+          }}
         />
+      </div>
+      {!user ? (
+        <div className="flex items-center gap-5">
+          <div className="font-extrabold text-lg">
+            사용을 위해 로그인을 해주세요.
+          </div>
+          <Button
+            URL="/sign-in"
+            label="로그인"
+            styleClass={"w-28 h-11 border-[3px] rounded-lg fontA text-ms"}
+          />
+        </div>
       ) : (
         <div className="flex items-center gap-5">
           <div className="font-extrabold text-lg">{user.name}</div>
