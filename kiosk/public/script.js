ac1ea98fb96bc8aea56e1750c251551fe64b9924
udongmandu <qjawns10000@naver.com>
2023-12-26 19:13:48 +0900

    키오스크 디자인 변경

--- kiosk/public/script.js
+++ kiosk/public/script.js
@@ -1,55 +1,9 @@
-// function sendBarcode() {
-//   const barcode = document.getElementById("barcode").value;
-
-//   fetch("서버 URL", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       barcode: barcode,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       document.getElementById("studentName").innerText = data.name;
-//       document.getElementById("studentBirth").innerText = data.birth;
-//       document.getElementById("studentInfo").style.display = "block";
-//     })
-//     .catch((error) => {
-//       console.error("서버와 통신할 수 없습니다.", error);
-//       alert("서버와 통신할 수 없습니다.");
-//     });
-// }
-
-// function checkIn(type) {
-//   const barcode = document.getElementById("barcode").value;
-
-//   fetch("서버 URL", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       barcode: barcode,
-//       type: type,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       alert(data.message);
-//     })
-//     .catch((error) => {
-//       console.error("서버와 통신할 수 없습니다.", error);
-//       alert("서버와 통신할 수 없습니다.");
-//     });
-// }
-
 const iconclick = document.querySelector(".menu_icon");
 let bar1 = document.querySelector(".bar_1_nonanima");
 let bar2 = document.querySelector(".bar_2_nonanima");
 let bar3 = document.querySelector(".bar_3_nonanima");
 let inner_box = document.querySelector(".innerBox");
+let spanBoxes = document.querySelectorAll(".spanBox");
 
 var toggle = 0;
 
@@ -59,12 +13,18 @@ function animation_on() {
     bar2.className = "bar_2_animated";
     bar3.className = "bar_3_animated";
     inner_box.className = "innerBox_animated";
+    spanBoxes.forEach(function (spanBox) {
+      spanBox.className = "spanBox-animated";
+    });
     toggle = 1;
   } else if (toggle == 1) {
     bar1.className = "bar_1";
     bar2.className = "bar_2";
     bar3.className = "bar_3";
     inner_box.className = "innerBox";
+    spanBoxes.forEach(function (spanBox) {
+      spanBox.className = "spanBox";
+    });
     toggle = 0;
   }
 }
