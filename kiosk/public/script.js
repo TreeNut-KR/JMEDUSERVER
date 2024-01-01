504e6e76c83146293bd2d5b8ab2cca66a9601b33
udongmandu <qjawns10000@naver.com>
2024-01-02 03:01:39 +0900

    키오스크 화면 동적 변환, 애니메이션 실행 조건 변경

--- kiosk/public/script.js
+++ kiosk/public/script.js
@@ -4,20 +4,42 @@ let bar2 = document.querySelector(".bar_2_nonanima");
 let bar3 = document.querySelector(".bar_3_nonanima");
 let inner_box = document.querySelector(".innerBox");
 let spanBoxes = document.querySelectorAll(".spanBox");
+let menu_icon = document.querySelector(".menu_icon");
+
+let testbutton = document.querySelector(".QRtest");
 
 var toggle = 0;
+let timer = null; // 타이머를 저장할 변수
 
+resizeHandler();
+
+// 박스 width, height 값 동일화
+function resizeHandler() {
+  const width = menu_icon.offsetWidth;
+
+  menu_icon.style.height = width + "px";
+}
+//윈도우 크기 변화 감지
+window.addEventListener("resize", resizeHandler);
+
+//상자 애니메이션 함수
 function animation_on() {
-  if (toggle == 0) {
-    bar1.className = "bar_1_animated";
-    bar2.className = "bar_2_animated";
-    bar3.className = "bar_3_animated";
-    inner_box.className = "innerBox_animated";
-    spanBoxes.forEach(function (spanBox) {
-      spanBox.className = "spanBox-animated";
-    });
-    toggle = 1;
-  } else if (toggle == 1) {
+  // 기존 타이머가 있으면 취소
+  if (timer) {
+    clearTimeout(timer);
+  }
+
+  // 애니메이션 시작
+  bar1.className = "bar_1_animated";
+  bar2.className = "bar_2_animated";
+  bar3.className = "bar_3_animated";
+  inner_box.className = "innerBox_animated";
+  spanBoxes.forEach(function (spanBox) {
+    spanBox.className = "spanBox-animated";
+  });
+
+  // 타이머 설정: 1초 후에 애니메이션 종료
+  timer = setTimeout(function () {
     bar1.className = "bar_1";
     bar2.className = "bar_2";
     bar3.className = "bar_3";
@@ -25,8 +47,7 @@ function animation_on() {
     spanBoxes.forEach(function (spanBox) {
       spanBox.className = "spanBox";
     });
-    toggle = 0;
-  }
+  }, 3000); // 3초 후에 애니메이션 종료
 }
-
-iconclick.addEventListener("click", animation_on);
+//버튼 테스트 (서비스 시 삭제)
+testbutton.addEventListener("click", animation_on);
