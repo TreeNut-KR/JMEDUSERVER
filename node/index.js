51d8bdc653ad3adb8b2092583599f2bbb9e71cd9
jgkim14 <48284333+jgkim14@users.noreply.github.com>
2024-01-02 18:19:05 +0900

    키오스크 도커화

--- node/index.js
+++ node/index.js
@@ -47,7 +47,7 @@ app.post('/login', (req, res) => {
         if (results.length > 0) {
             bcrypt.compare(password, results[0].pwd, (err, isMatch) => {
                 if (err) {
-                    res.status(500).json({ success: false, message: '서버 에러'});
+                    res.status(500).json({ success: false, message: '서버 에러입니다. 관리자에게 문의하십니오.'});
                     return;
                 }
 
@@ -69,21 +69,29 @@ app.post('/login', (req, res) => {
 app.post('/register', async(req, res) => {
     const { name, id, pwd, sex_ism, birthday, contact, is_admin } = req.body;
     console.log('가입 요청 들어옴');
-
-
     const hashedPassword = await bcrypt.hash(pwd, 10); // 비밀번호 해싱
-  
-    // 데이터 삽입 쿼리
-    const query = 'INSERT INTO teacher (teacher_pk, name, id, pwd, sex_ism, birthday, contact, is_admin) VALUES (UUID(), ?, ?, ?, ?, ?, ?, ?)';
-  
-    // 데이터베이스에 쿼리 실행
-    db.query(query, [name, id, hashedPassword, sex_ism, birthday, contact, is_admin], (err, result) => {
-      if (err) {
-        console.error('데이터 삽입 중 오류 발생:', err);
-        res.status(500).send('서버 오류가 발생했습니다.');
-        return;
-      }
-      res.status(200).send('사용자가 성공적으로 등록되었습니다.');
+
+
+    db.query('SELECT * FROM teacher WHERE id = ?', [username], (err, results) => {
+        if(results.length)
+        {
+            res.status(200).send('이미 사용중인 ID입니다.');
+        }
+        else
+        {
+            // 데이터 삽입 쿼리
+            const query = 'INSERT INTO teacher (teacher_pk, name, id, pwd, sex_ism, birthday, contact, is_admin) VALUES (UUID(), ?, ?, ?, ?, ?, ?, ?)';
+          
+            // 데이터베이스에 쿼리 실행
+            db.query(query, [name, id, hashedPassword, sex_ism, birthday, contact, is_admin], (err, result) => {
+              if (err) {
+                console.error('데이터 삽입 중 오류 발생:', err);
+                res.status(500).send('서버 오류가 발생했습니다.');
+                return;
+              }
+              res.status(200).send('가입 완료.');
+            });
+        }
     });
   });
 
