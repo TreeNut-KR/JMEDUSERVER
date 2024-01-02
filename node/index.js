2c970863b0d6652703e02683253e3fa2c84935a7
jgkim14 <48284333+jgkim14@users.noreply.github.com>
2024-01-02 09:03:23 +0900

    회원가입 정상, 로그인 정상

--- node/index.js
+++ node/index.js
@@ -66,14 +66,18 @@ app.post('/login', (req, res) => {
 
 
 //////회원가입
-app.post('/register', (req, res) => {
+app.post('/register', async(req, res) => {
     const { name, id, pwd, sex_ism, birthday, contact, is_admin } = req.body;
+    console.log('가입 요청 들어옴');
+
+
+    const hashedPassword = await bcrypt.hash(pwd, 10); // 비밀번호 해싱
   
     // 데이터 삽입 쿼리
     const query = 'INSERT INTO teacher (teacher_pk, name, id, pwd, sex_ism, birthday, contact, is_admin) VALUES (UUID(), ?, ?, ?, ?, ?, ?, ?)';
   
     // 데이터베이스에 쿼리 실행
-    db.query(query, [name, id, pwd, sex_ism, birthday, contact, is_admin], (err, result) => {
+    db.query(query, [name, id, hashedPassword, sex_ism, birthday, contact, is_admin], (err, result) => {
       if (err) {
         console.error('데이터 삽입 중 오류 발생:', err);
         res.status(500).send('서버 오류가 발생했습니다.');
@@ -86,25 +90,37 @@ app.post('/register', (req, res) => {
 
 
 /////////////////////학생조회
-app.get('/student_view', (req, res) => {
+app.get('/students_view', (req, res) => {
     // 'student' 테이블의 모든 데이터를 조회
-    db.query('SELECT * FROM student', async (error, results, fields) => {
-        if (error) throw error;
-
-        try {
-            // POST 요청으로 데이터 전송
-            const response = await axios.post('react:5001', results);
-            res.send('Data sent successfully: ' + response.data);
-        } catch (error) {
-            res.status(500).send('Error sending data: ' + error.message);
+    db.query('SELECT * FROM student', (error, results, fields) => {
+        if (error) {
+            // 데이터베이스 쿼리 중 오류 발생 시
+            res.status(500).json({ success: false, message: '데이터베이스 오류', error: error });
+        } else {
+            // 쿼리가 성공적으로 수행되면 결과 반환
+            res.json({ success: true, students: results });
         }
     });
 });
 
 
-app.post('/student_edit', (req, res) => {
-
-});
+///////학생추가
+app.post('/students_add', (req, res) => {
+    const { name, sex_ism, birthday, contact, contact_parent, school, payday, firstreg } = req.body;
+  
+    // 데이터 삽입 쿼리
+    const query = 'INSERT INTO student (student_pk, name, sex_ism, birthday, contact, contact_parent, school, payday, firstreg) VALUES (UUID(), ?, ?, ?, ?, ?, ?, ?, ?)';
+  
+    // 데이터베이스에 쿼리 실행
+    db.query(query, [name, sex_ism, birthday, contact, contact_parent, school, payday, firstreg], (err, result) => {
+      if (err) {
+        console.error('데이터 삽입 중 오류 발생:', err);
+        res.status(500).send('서버 오류가 발생했습니다.');
+        return;
+      }
+      res.status(200).send('사용자가 성공적으로 등록되었습니다.');
+    });
+  });
 
 
 
@@ -123,6 +139,52 @@ app.get('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/login');
 });
+
+/////키오스크에서 학생키를 받은 경우
+app.post('/QR', (req, res) => {
+    const { qrcode } = req.body;
+    db.query('SELECT * FROM student WHERE student_pk = ?', [qrcode], (err, results) => {
+        if (err) {
+            res.status(500).json({ success: false, message: '서버에러'});
+            return;
+        }
+
+        if (results.length > 0) {
+            const name = results[0].name;
+            const birthday = results[0].birthday;
+            const contact_parent = results[0].contact_parent;
+            res.json({ success: true, name: name, birthday: birthday, contact_parent: contact_parent});
+
+        } else {
+            res.json({ success: false, message: '없는 ID입니다.'});
+        }
+    });
+});
+
+/////문자API 전송 요청
+app.post('/kiost_sendmsg', (req, res) => {
+    const { name, contact_parent, attend_code } = req.body;
+    const msg = name+'학생이 지금 ';
+    if (attend_code == 0)
+    {
+        msg = msg + '등원하였습니다.';
+    }
+    else if(attend_code ==1)
+    {
+        msg = msg + '하원하였습니다.';
+    }
+    try {
+        const response = axios.post('http://localhost:5100/input', {
+            msg,
+            contact_parent
+        });
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+    }
+});
+
+
 
 // 서버 시작
 app.listen(5002, () => {
