67449881746e1caff8d8aa79cadeb9e9ef7d1636
jgkim14 <48284333+jgkim14@users.noreply.github.com>
2024-01-11 12:52:54 +0900

    조건부공지 전송기능, 학교관리 기능 추가

--- node/sources/notification.js
+++ node/sources/notification.js
@@ -6,24 +6,30 @@ const router = express.Router();
 
 
 //조건부 공지
-router.post("/condition_note", (req, res) => {
-    const { name, sex_ism, contact, contact_parent, school, payday } = req.body;
-    const selectAll = true;
-  
-    // 'student' 테이블 조회 쿼리문 넣기
-    db.query(searchStudent, (error, results, fields) => {
+router.post("/conditional_note", async (req, res) => { // 여기에 async 추가
+    let query = makeStudentSearchQuery(req.body);
+    db.query(query, async (error, results) => { // 만약 여기서도 await를 사용한다면 async 추가
       if (error) {
-        // 데이터베이스 쿼리 중 오류 발생 시
-        res
-          .status(500)
-          .json({ success: false, message: "데이터베이스 오류", error: error });
+        res.status(500).json({ success: false, message: "데이터베이스 오류" });
       } else {
-        // 쿼리가 성공적으로 수행되면 결과 반환
-        res.json({ success: true, students: results });
+        for (var i = 0; i < results.length; i++) {
+            try {
+                const contact_parent = results[i].contact_parent;
+                const name = results[i].name;
+                const response = await axios.post("http://localhost:5100/input", {
+                    contact_parent,
+                    name,
+                    msg,
+                });
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        res.json({ success: true, message: 완료 });
       }
     });
-  });
-  
+});
 
 
 
