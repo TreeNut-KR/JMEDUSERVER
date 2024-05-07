4f25ff14bfd267ac76c01bdb876644888429900c
udongmandu <qjawns10000@naver.com>
2024-05-07 17:51:51 +0900

    검색 필터 교사용 추가

--- react/sources/src/constants/searchFilter.js
+++ react/sources/src/constants/searchFilter.js
@@ -9,7 +9,22 @@ export const SEARCH_STUDENT = {
     value: "contact",
   },
 };
-
+//강사 검색 필터
+export const SEARCH_TEACHER = {
+  name: {
+    name: "이름",
+    value: "name",
+  },
+  phoneNumber: {
+    name: "핸드폰 번호",
+    value: "contact",
+  },
+  test: {
+    name: "test",
+    value: "contact",
+  },
+};
+//학생 컬럼
 export const EDIT_STUDENT = {
   name: {
     name: "이름",
@@ -50,6 +65,34 @@ export const EDIT_STUDENT = {
     name: "??뭘 날",
     type: "date",
     value: "firstreg",
+  },
+  remove: {
+    name: "삭제하기",
+    type: "text",
+    value: "remove",
+  },
+};
+//강사 컬럼
+export const EDIT_TEACHER = {
+  name: {
+    name: "이름",
+    type: "text",
+    value: "name",
+  },
+  sex_ism: {
+    name: "성별",
+    type: "radio",
+    value: "sex_ism",
+  },
+  birthday: {
+    name: "생일",
+    type: "date",
+    value: "birthday",
+  },
+  contact: {
+    name: "연락처 (개인)",
+    type: "phone",
+    value: "contact",
   },
   remove: {
     name: "삭제하기",
