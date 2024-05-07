746e61253f7ac4699cd722a191c7077966c49cc5
udongmandu <qjawns10000@naver.com>
2024-05-07 17:51:08 +0900

    type 에 따른 반응 추가

--- react/sources/src/Components/dataTableV1/DataTableV1.jsx
+++ react/sources/src/Components/dataTableV1/DataTableV1.jsx
@@ -14,13 +14,14 @@ export default function DataTableV1(props) {
   const [selectedCheckboxes, setSelectedCheckboxes] = useState([]);
   const [selectedType, setSelectedType] = useState({});
   const [editTextDB, setEditTextDB] = useState("");
-  const [studnetArray, setStudentArray] = useState([]);
+  const [dataArray, setDataArray] = useState([]);
   const [editText, setEditText] = useState({
     text: "",
     option: "",
     submit: false,
   });
 
+  //다수 개체 한번에 수정 위한 버튼
   function editAllButton() {
     setEditText({
       text: editTextDB,
@@ -28,41 +29,48 @@ export default function DataTableV1(props) {
       submit: true,
     });
     if (selectedType.value === "remove") setEditText({ text: "remove" });
-    if (editText.text && studnetArray.length >= 1 && editText.submit) {
-      runSQL(editText, studnetArray);
+    if (editText.text && dataArray.length >= 1 && editText.submit) {
+      runSQL(editText, dataArray);
       setEditText({ text: "", option: "", submit: false });
     }
   }
 
+  //data 안의 pk 값 이름 재설정
+  const modifiedData = datas.map((item) => {
+    return {
+      ...item,
+      data_pk: item[type + "_pk"],
+    };
+  });
+
+  //에드온 버튼
   const handleMasterCheckboxChange = (e) => {
     const checked = e.target.checked;
     let newDataToSelect = [];
 
     if (checked) {
-      newDataToSelect = currentPageData.map((item) => item.student_pk);
+      newDataToSelect = currentPageData.map((item) => item.data_pk);
     }
 
     setSelectedCheckboxes(newDataToSelect);
   };
 
-  const handleCheckboxChange = (e, studentPk) => {
+  const handleCheckboxChange = (e, dataPk) => {
     const checked = e.target.checked;
 
     let newSelectedCheckboxes = [...selectedCheckboxes];
 
     if (checked) {
-      newSelectedCheckboxes.push(studentPk);
+      newSelectedCheckboxes.push(dataPk);
     } else {
-      newSelectedCheckboxes = newSelectedCheckboxes.filter(
-        (pk) => pk !== studentPk
-      );
+      newSelectedCheckboxes = newSelectedCheckboxes.filter((pk) => pk !== dataPk);
     }
 
     setSelectedCheckboxes(newSelectedCheckboxes);
   };
 
   useEffect(() => {
-    setStudentArray(selectedCheckboxes);
+    setDataArray(selectedCheckboxes);
   }, [selectedCheckboxes]);
 
   //자세히 보기 기능 ---------------------------------
@@ -70,16 +78,11 @@ export default function DataTableV1(props) {
 
   const [itemsPerPage, setItemPerPage] = useState(10);
 
-  const totalPages = Math.ceil(
-    parseInt(totalNumber, 10) / parseInt(itemsPerPage, 10)
-  );
+  const totalPages = Math.ceil(parseInt(totalNumber, 10) / parseInt(itemsPerPage, 10));
 
-  const startIndex =
-    (parseInt(currentPage, 10) - 1) * parseInt(itemsPerPage, 10);
+  const startIndex = (parseInt(currentPage, 10) - 1) * parseInt(itemsPerPage, 10);
   const endIndex = parseInt(startIndex, 10) + parseInt(itemsPerPage, 10);
-  const currentPageData = datas
-    ? datas.slice(parseInt(startIndex, 10), parseInt(endIndex, 10))
-    : [];
+  const currentPageData = modifiedData ? modifiedData.slice(parseInt(startIndex, 10), parseInt(endIndex, 10)) : [];
 
   //테이블 내의 '자세히 보기' 버튼 뜨도록 하는 기능 -------
   const toggleRowExpansion = (index) => {
@@ -143,31 +146,27 @@ export default function DataTableV1(props) {
               </option>
             ))}
           </select>
-          <InputBox
-            data={editTextDB}
-            edit={setEditTextDB}
-            type={selectedType.type}
-            options={["남", "여"]}
-          />
+          <InputBox data={editTextDB} edit={setEditTextDB} type={selectedType.type} options={["남", "여"]} />
           <div className="flex justify-end py-3">
             <Button label={"수정"} onClick={editAllButton} />
           </div>
         </div>
       ) : null}
       <div className="border border-[#B3A492] rounded-md">
-        <table
-          className={`${styleClass} border-collapse rounded-md text-sm shadow-md w-full fontA `}
-        >
+        <table className={`${styleClass} border-collapse rounded-md text-sm shadow-md w-full fontA `}>
           <caption className="text-left font-extrabold px-3 text-lg">
-            {title} |{" "}
-            <span className="text-sm pr-4"> 전체 : {totalNumber}</span>
-            <Button
-              // type 종류에 따라 URL 변경 할 수 있도록 수정 해야함 (아직 type 종류 지정해 놓지 않아서 냅둠)
-              URL={"/student-add"}
-              width={130}
-              height={30}
-              label={"정보 추가하기"}
-            />
+            {title} | <span className="text-sm pr-4"> 전체 : {totalNumber}</span>
+            {type === "teacher" ? (
+              <Button
+                URL={`/${type}-add`}
+                width={300}
+                height={30}
+                disabled={true}
+                label={"강사는 회원가입을 통해 추가해주세요"}
+              />
+            ) : (
+              <Button URL={`/${type}-add`} width={130} height={30} label={"정보 추가하기"} />
+            )}
           </caption>
 
           <thead className="text-base font-semibold">
@@ -176,10 +175,7 @@ export default function DataTableV1(props) {
                 <td className="pl-3" key={"index"}>
                   <input
                     type="checkbox"
-                    checked={
-                      selectedCheckboxes.length === currentPageData.length &&
-                      selectedCheckboxes.length > 0
-                    }
+                    checked={selectedCheckboxes.length === currentPageData.length && selectedCheckboxes.length > 0}
                     onChange={(e) => handleMasterCheckboxChange(e)}
                   />
                 </td>
@@ -194,17 +190,15 @@ export default function DataTableV1(props) {
           {totalNumber > 0 ? (
             <tbody className="px-3">
               {currentPageData.map((item, index) => (
-                <React.Fragment key={item.student_pk}>
+                <React.Fragment key={item.data_pk}>
                   <tr className="relative">
                     {editAll ? (
                       <td className="pl-3">
                         <input
                           type="checkbox"
                           className="relative"
-                          checked={selectedCheckboxes.includes(item.student_pk)}
-                          onChange={(e) =>
-                            handleCheckboxChange(e, item.student_pk)
-                          }
+                          checked={selectedCheckboxes.includes(item.data_pk)}
+                          onChange={(e) => handleCheckboxChange(e, item.data_pk)}
                         />
                       </td>
                     ) : null}
@@ -227,7 +221,7 @@ export default function DataTableV1(props) {
                         {expandedRowIndex === index && (
                           <span
                             className="absolute top-[-4px] left-[7rem] transform translate-x-[-50%] bg-[#5272F2] px-3 py-1 border rounded w-[10rem] text-white"
-                            onClick={() => buttonEffect(item.student_pk)}
+                            onClick={() => buttonEffect(item.data_pk)}
                           >
                             자세히 보기 / 수정
                           </span>
@@ -256,12 +250,7 @@ export default function DataTableV1(props) {
             </select>
           </div>
           <div className="flex items-center border-2 rounded-md border-[#5272F2] px-2">
-            <input
-              className="h-4 w-4"
-              type="checkbox"
-              checked={editAll}
-              onChange={() => setEditAll(!editAll)}
-            />
+            <input className="h-4 w-4" type="checkbox" checked={editAll} onChange={() => setEditAll(!editAll)} />
             <label className="pl-3" for="scales">
               한번에 수정하기
             </label>
@@ -292,9 +281,7 @@ export default function DataTableV1(props) {
                   key={index}
                   onClick={() => setCurrentPage(pageNumber)}
                   className={`px-2 mx-1 rounded-lg border text-xs h-7 w-7 select-none ${
-                    currentPage === pageNumber
-                      ? "bg-[#5272F2] text-white"
-                      : "bg-white"
+                    currentPage === pageNumber ? "bg-[#5272F2] text-white" : "bg-white"
                   }`}
                 >
                   {pageNumber}
@@ -332,7 +319,7 @@ DataTableV1.propTypes = {
   styleClass: PropTypes.string,
   columns: PropTypes.array.isRequired,
   type: PropTypes.string.isRequired,
-  setStudentArray: PropTypes.func,
+  setDataArray: PropTypes.func,
   editType: PropTypes.object,
   setEditText: PropTypes.func,
 };
